dac2640bb90d527e464c348131640958296cd15d
TranVux <vutran789jjjj@gmail.com>
2024-03-24 10:24:12 +0700

    feat: add base activity-fragment ⚒️

---
+++ app/src/main/java/com/youthtech/rhythmify/ui/fragments/BlankFragment.kt
@@ -0,0 +1,14 @@
+package com.youthtech.rhythmify.ui.fragments
+
+import com.youthtech.rhythmify.databinding.FragmentBlankBinding
+import com.youthtech.rhythmify.ui.base.BaseFragment
+
+class BlankFragment : BaseFragment<FragmentBlankBinding>(FragmentBlankBinding::inflate) {
+    override fun init() {
+
+    }
+
+    override fun addListener() {
+
+    }
+}
