07ac30ecaae1b2bf1a956b8fe6e95331755224e1
TranVux <vutran789jjjj@gmail.com>
2024-03-24 18:35:45 +0700

    chore: add YoutubeSearchDTO ⚒️

--- app/src/main/java/com/youthtech/rhythmify/data/apis/YoutubeService.kt
+++ app/src/main/java/com/youthtech/rhythmify/data/apis/YoutubeService.kt
@@ -1,5 +1,6 @@
 package com.youthtech.rhythmify.data.apis
 
+import com.youthtech.rhythmify.data.dto.YoutubeSearchResponse
 import com.youthtech.rhythmify.data.music_service.ApiPath
 import com.youthtech.rhythmify.utils.YOUTUBE_API_KEY
 import retrofit2.Response
@@ -13,5 +14,5 @@ interface YoutubeService {
         @Query("key") apiKey: String? = YOUTUBE_API_KEY,
         @Query("type") type: String? = "video",
         @Query("videoCategoryId") videoCategoryId: String? = "20"
-    ): Response<Any>
+    ): Response<YoutubeSearchResponse>
 }
