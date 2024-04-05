340ef7c3ee27c37db8b263ae43de233d72ace518
duyrk <99589013+duyrk@users.noreply.github.com>
2024-04-05 16:48:02 +0700

    feat: add listsection component

---
+++ app/src/main/java/com/youthtech/rhythmify/ui/components/ListSection.kt
@@ -0,0 +1,78 @@
+package com.youthtech.rhythmify.ui.components
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.youthtech.rhythmify.R
+
+class ListSection : LinearLayout, View.OnClickListener {
+    private lateinit var textViewSectionName: TextView
+    private lateinit var icWatchMore: ImageView
+    private lateinit var recyclerView: RecyclerView
+    private var watchMoreHandler: WatchMoreButtonHandler? = null
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        LayoutInflater.from(context).inflate(R.layout.component_list_section, this);
+        val style = getContext().theme.obtainStyledAttributes(attrs, R.styleable.ListSection, 0, 0);
+        init(style)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr){
+        LayoutInflater.from(context).inflate(R.layout.component_list_section, this);
+        val style = getContext().theme.obtainStyledAttributes(attrs, R.styleable.ListSection, defStyleAttr, 0);
+        init(style)
+    }
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes){
+        LayoutInflater.from(context).inflate(R.layout.component_list_section, this);
+        val style = getContext().theme.obtainStyledAttributes(attrs, R.styleable.ListSection, defStyleAttr, defStyleRes);
+        init(style)
+    }
+    fun init(style: TypedArray?) {
+        textViewSectionName = findViewById(R.id.tv_sectionName)
+        icWatchMore = findViewById(R.id.iv_watchmore)
+        recyclerView = findViewById(R.id.rv_songList)
+        icWatchMore.setOnClickListener(this@ListSection)
+        if (style == null) return
+        try {
+            val sectionName = style.getString(R.styleable.ListSection_sectionName)
+            val moreButton = style.getBoolean(R.styleable.ListSection_moreButton, true);
+            textViewSectionName.text = sectionName;
+            icWatchMore.visibility = if (moreButton) VISIBLE else GONE
+        } catch (e: Exception) {
+            Log.d(TAG, "listsection init: ${e.message}")
+        } finally {
+            style.recycle()
+        }
+
+    }
+    fun setOnWatchMoreListener(watchMoreHandler: WatchMoreButtonHandler) {
+        this.watchMoreHandler = watchMoreHandler
+        requestLayout()
+    }
+    fun setSectionName(name: String){
+        textViewSectionName.text = name
+        requestLayout()
+    }
+    fun setAdapter(adapter: RecyclerView.Adapter<*>){
+        recyclerView.adapter = adapter
+        requestLayout()
+    }
+    override fun onClick(v: View) {
+        if(v.id == R.id.iv_watchmore && watchMoreHandler != null){
+            watchMoreHandler!!.onMoreButtonClick()
+        }
+    }
+    companion object {
+        private val TAG = ListSection::class.java.simpleName
+    }
+    interface WatchMoreButtonHandler {
+        fun onMoreButtonClick()
+    }
+}
+
--- app/src/main/java/com/youthtech/rhythmify/ui/fragments/ExploreFragment.kt
+++ app/src/main/java/com/youthtech/rhythmify/ui/fragments/ExploreFragment.kt
@@ -1,12 +1,7 @@
 package com.youthtech.rhythmify.ui.fragments
 
-import android.util.Log
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.LinearLayout.LayoutParams
-import androidx.core.view.marginLeft
-import androidx.core.view.updateLayoutParams
 import com.google.android.material.chip.Chip
 import com.youthtech.rhythmify.R
 import com.youthtech.rhythmify.databinding.FragmentExploreBinding
@@ -15,16 +10,18 @@ import com.youthtech.rhythmify.ui.base.BaseFragment
 class ExploreFragment : BaseFragment<FragmentExploreBinding>(FragmentExploreBinding::inflate) {
     val dummyGenre = ArrayList<String>();
     override fun init() {
-    renderGenre()
-  
+        renderGenre()
+
     }
 
     override fun addListener() {
-        binding.chipGrGenre.setOnCheckedStateChangeListener { chipGroup, ints ->
+        binding.chipGrGenre.setOnCheckedStateChangeListener { _, ints ->
             println(dummyGenre[ints[0]])
         }
     }
-    fun renderGenre(){
+
+    @SuppressLint("InflateParams")
+    fun renderGenre() {
         dummyGenre.add("Kpop")
         dummyGenre.add("hiphop")
         dummyGenre.add("Rap")
@@ -35,18 +32,6 @@ class ExploreFragment : BaseFragment<FragmentExploreBinding>(FragmentExploreBind
         dummyGenre.add("sdsdsdsdsd")
         dummyGenre.forEachIndexed { index, s ->
             val chip: Chip = LayoutInflater.from(context).inflate(R.layout.item_genre, null) as Chip
-//            if(index == 0){
-//                Log.d(TAG, "index $index")
-//                val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-//                layoutParams.setMargins(20,0,0,0);
-//                chip.layoutParams = layoutParams
-//            }
-//            if(index == dummyGenre.size - 1){
-//                Log.d(TAG, "index $index")
-//                val layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-//                layoutParams.setMargins(0,0,20,0);
-//                chip.layoutParams = layoutParams
-//            }
             chip.text = s;
             chip.id = index
             binding.chipGrGenre.addView(chip)
